48db9cea43c04b795b8c8fcc55158ef3707c2a33
hanbin <hnbn707@gmail.com>
2022-05-21 21:46:31 +0900

    test2

---
+++ src/InnerApp.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function InnerApp() {
+  return (
+    <View>
+      <Text>Hi react native Inner App</Text>
+    </View>
+  );
+}
+
+export default InnerApp;
