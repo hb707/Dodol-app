9f1b8974a0fd9ff72e713b84a32350a7fa2fdc10
jihyun kim <jihyunivy@gmail.com>
2022-05-24 18:49:11 +0900

    [login]:wip

--- src/Screens/ThumbPicker.tsx
+++ src/Screens/ThumbPicker.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button, Image, Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Fontisto } from '@expo/vector-icons';
