3e052a01c007150f04fd669ab941e6c6ccd32c8a
dev_kong <ahdjd5@gmail.com>
2022-05-30 15:53:43 +0900

    [loading]feat: login page done

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@
 import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { Asset } from 'expo-asset';
+import Loading from './src/Components/loading/loading';
 import Route from './src/Components/Route/Route';
 import Store from './src/Store/Store';
 
@@ -14,6 +15,12 @@ function App() {
     await Asset.loadAsync([
       require('./assets/dodol_bg_list.jpg'),
       require('./assets/Home/jar.png'),
+      require('./assets/capsule_thumb_frame.png'),
+      require('./assets/default_capsule_thumbnail.png'),
+      require('./assets/capsule_title_bg.png'),
+      require('./assets/capsule_date_bg.png'),
+      require('./assets/polaroid.png'),
+      require('./assets/paper.jpeg'),
     ]);
   };
 
@@ -22,14 +29,14 @@ function App() {
       await getImg();
       setTimeout(() => {
         setisLoading(false);
-      }, 2000);
+      }, 2500);
     })();
   });
 
   return (
     <>
       {isLoading ? (
-        <Text>로딩페이지</Text>
+        <Loading />
       ) : (
         <Store>
           <Route />
